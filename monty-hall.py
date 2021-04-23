9c171f3876e358d58055748ebea42e8eec3c5800
brian <brian@pop-os.localdomain>
2021-04-22 22:04:37 -0400

    removed bad code and bad logging

--- monty-hall.py
+++ monty-hall.py
@@ -1,39 +1,20 @@
 #!/usr/bin/env python
 
-import os
 import random
 
-path = "database.txt"
-database = open(path, "r+")
-stays = switches = switches_right = stays_right = line_num = 0
-
-if os.stat(path).st_size > 0 :
-  # read the results from the file
-  for line in database.readlines():
-    line_num += 1
-    # Line 2 is stays
-    if (line_num == 2) :
-      stays = int(line)
-    # Line 3 is stays
-    if (line_num == 3) :
-      switches = int(line)
-    # Line 4 is switches that were correct
-    if (line_num == 4) :
-      switches_right = int(line)
-    # Line 5 is stays that were right
-    if (line_num == 5) :
-      stays_right = int(line)
-  # end for loop
-# end if statement
-
-# calculate total (for use in percentages)
-total_rounds = stays + switches
+opened_door = 0
 
 # Create our door options
-doors = [1, 2, 3]
+prize = random.randint(1,3)
+doors = {'1': 0 , '2': 0, '3': 0 }
 
-# Pick a door at random to be the 'prize door'
-prize = random.choice(doors)
+for key,value in doors.items():
+    if prize == int(key):
+        doors[key] = 'prize'
+    else:
+        doors[key] = 'goat'
+
+
 
 print("There is a prize behind on of these three doors...")
 print("   [1]   [2]   [3]")
@@ -42,37 +23,20 @@ print("   [1]   [2]   [3]")
 first_choice = input("What door is the prize behind? ")
 print("you picked door #" + str(first_choice))
 
-# Give the user the option to switch
-# Fist tho, remove the prize door from our list of options
-doors.pop(prize-1)
 
-# If the user didn't pick the prize door, make sure the door they choice is
-# removed from the options
-if first_choice != prize :
-  doors.pop(first_choice-1)
+for key,value in doors.items():
+    if first_choice == key:
+        pass
+    else:
+        if value == 'goat':
+            opened_door = key
+            continue
 
-# Pick a goat door that is not the prize door and is not the user's pick.
-goat = random.choice(doors)
-#remove the goat door from the list of options.
-doors.pop(goat-1)
-print("There is a goat behind door # " + str(goat))
 
-# get the last door number.
-last = doors.pop()
+print("There is a goat behind door # " + str(opened_door))
+
+
 # Ask the user if they want to switch
-switch = input("Do you want to switch your pick from door #" + str(first_choice) + " to door #" + str(last) + "? (y/n)")
+switch = input("Do you want to switch your pick from door #" + str(first_choice) + "? If so pick a door ")
 
-# @todo compute results
- @todo log results
-
-# Re-open the file for writing so that it is overwritten.
-database = open(path, 'w')
-# write the results to the file
-database.write("Results file. Line 2 is stays, 3 is switches, line 4 is switches that turned out to be right, and line 5 is stays that turned out to be right. \n")
-database.write(stays)
-database.write(switches)
-database.write(switches_right)
-database.write(stays_right)
-database.close()
-
-# comment
+print(f"The door you picked contains a {doors[switch]}")
